27524c82680cad6213497495e1e77dc11e02dccf
Виталий <its.vint@ya.ru>
2020-11-30 13:33:25 +0300

    Initial Commit

---
+++ Homework2_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework2_SwiftUI
+//
+//  Created by Виталий Овсянников on 30.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homework2_SwiftUI/Homework2_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2_SwiftUIApp.swift
+//  Homework2_SwiftUI
+//
+//  Created by Виталий Овсянников on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Homework2_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
